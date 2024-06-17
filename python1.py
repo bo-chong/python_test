ccb0472876df7ee46d51d64aa06e665ba019c382
bryce <1016997319@qq.com>
2024-06-17 16:36:37 +0800

    update python1

---
+++ python1.py
@@ -0,0 +1,11 @@
+#！usr/bin/python3
+# Print a greeting message to the console
+'''
+print("Hello, World!")
+'''
+a=1+1+ \
+  1+1 
+print("Hello, World!")
+print(a)
+complex_number = 3 + 4j
+print(complex_number)
