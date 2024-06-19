57a8952a0306c34a0a4875379a39d0dab8dc0142
bryce <1016997319@qq.com>
2024-06-19 09:17:17 +0800

    update python

--- python1.py
+++ python1.py
@@ -9,3 +9,8 @@ print("Hello, World!")
 print(a)
 complex_number = 3 + 1j
 print(complex_number)
+print(r"this is a line with \n")
+str='123456789'
+print(str[0:-2])
+print(str[1:8:2])
+input("\n\n按下enter键退出...")
