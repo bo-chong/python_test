352b5e3a83eabc82cc22a90e9fcd0297557984c0
bryce <1016997319@qq.com>
2024-06-26 17:34:30 +0800

    py

--- python1.py
+++ python1.py
@@ -1,18 +1,53 @@
 #！usr/bin/python3
 # Print a greeting message to the console
-'''
-print("Hello, World!")
-'''
-a=1+1+ \
-  1+1 
-print("Hello, World!")
-print(a)
-complex_number = 3 + 1j
-print(complex_number)
-print(r"this is a line with \n")
-str='123456789'
-print(str[0:-2])
-print(str[1:8:2])
-input("\n\n按下enter键退出...")
-a = 111
-print(a)
+import time
+# for i in range(101):
+#     print("\r{:3}%".format(i),end='')
+#     time.sleep(0.05)
+# Number String Tuple (不可变数据)
+# List Dictionary Set (可变数据）
+a , b, c, d =20, 5.5, True,4+3j
+print(type(a),type(b),type(c),type(d))
+
+str = 'mypython'
+print(str[0:2])
+print(str[0:-1])
+print(str+'TEST')
+
+t = ["a",'b','c',"d"]
+i = ["e","f","g"]
+print(t)
+t[1] = 'x'
+print(t)
+
+tuple = (1,2,3,4,5)
+print(tuple[1:3])
+print(tuple[2:])
+
+sites_1 = {'a','b','c','d'}
+sites_2 = {'e','f','g','d'}
+print(sites_1)
+print(sites_1-sites_2)
+print(sites_1 | sites_2)
+print(sites_1 & sites_2)
+print(sites_1 ^ sites_2)
+
+dic = {'name':'John', 'age':30, 'city':'New York'}
+print(dic['name'])
+print(dic.keys())
+print(dic.values())
+print(dic.items())
+
+x = b'hello'
+y = x[1:3]
+z = x + b'world'
+print(x)
+print(y)
+print(z)
+
+n = 10
+if n>5:
+    print(n)
+if (n:=10) > 5:
+    print(n)
+# := 等于 == and =
