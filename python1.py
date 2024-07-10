19ac30cac96280c40cdf9633974901234ea64026
bryce <1016997319@qq.com>
2024-07-10 08:41:59 +0800

    py

--- python1.py
+++ python1.py
@@ -8,23 +8,36 @@ import time
 # List Dictionary Set (可变数据）
 a , b, c, d =20, 5.5, True,4+3j
 print(type(a),type(b),type(c),type(d))
-
-str = 'mypython'
+print("string------------------------------------------------")
+str = 'mypython' # 字符串
 print(str[0:2])
 print(str[0:-1])
 print(str+'TEST')
 
-t = ["a",'b','c',"d"]
+print("list------------------------------------------------")
+muitiples = [i for i in range(30) if i%3==0] # 列表推导式
+print(muitiples)
+
+t = ["a",'b','c',"d"] # 列表
 i = ["e","f","g"]
 print(t)
 t[1] = 'x'
 print(t)
+it = iter(t)
+print(next(it))
+print(next(it))
+print(next(it))
 
-tuple = (1,2,3,4,5)
+print("tuple------------------------------------------------")
+tuple = (1,2,3,4,5) # 元组
 print(tuple[1:3])
 print(tuple[2:])
 
-sites_1 = {'a','b','c','d'}
+a =(tuple(x) for x in range(10)) # 元组推导式
+print(a)
+
+print("set------------------------------------------------")
+sites_1 = {'a','b','c','d'} # 集合
 sites_2 = {'e','f','g','d'}
 print(sites_1)
 print(sites_1-sites_2)
@@ -32,11 +45,18 @@ print(sites_1 | sites_2)
 print(sites_1 & sites_2)
 print(sites_1 ^ sites_2)
 
-dic = {'name':'John', 'age':30, 'city':'New York'}
+setnew ={i**2 for i in range(4)} # 集合推导式
+print(setnew)
+
+print("dictionary------------------------------------------------")
+dic = {'name':'John', 'age':30, 'city':'New York'} # 字典
 print(dic['name'])
 print(dic.keys())
 print(dic.values())
 print(dic.items())
+
+dic1 = {x: x**2 for x in range(1,6)} # 字典推导式
+print(dic1)
 
 x = b'hello'
 y = x[1:3]
@@ -51,3 +71,10 @@ if n>5:
 if (n:=10) > 5:
     print(n)
 # := 等于 == and =
+
+a = 10
+if a > 5:
+    print(a)
+else:
+    print("a is less than 5")
+
